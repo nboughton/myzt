a6ed9b5a31760cad12549371527e4e500d67ecb5
Nick Boughton <nicholasboughton@gmail.com>
2019-12-04 14:01:09 +0000

    initial commit

---
+++ cmd/d66.go
@@ -0,0 +1,44 @@
+// Copyright © 2019 Nick Boughton <nicholasboughton@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/nboughton/go-roll"
+	"github.com/spf13/cobra"
+)
+
+// d66Cmd represents the d66 command
+var d66Cmd = &cobra.Command{
+	Use:   "d66",
+	Short: "Roll d66",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		rTens, rDigits := roll.D6.Roll(), roll.D6.Roll()
+
+		fmt.Printf("%s%s\n", rTens.Value, rDigits.Value)
+	},
+}
+
+func init() {
+	rollCmd.AddCommand(d66Cmd)
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/nboughton/myzt
+
+go 1.13
+
+require (
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/nboughton/go-roll v0.0.12
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.5.0
+)
