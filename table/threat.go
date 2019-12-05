14f80aed6380932184f1ea2ac7e26b5581a3ea67
Nick Boughton <nicholasboughton@gmail.com>
2019-12-05 14:40:23 +0000

    Added threat

---
+++ table/threat.go
@@ -0,0 +1,34 @@
+package table
+
+import "github.com/nboughton/go-roll"
+
+// Threat table
+var Threat = roll.List{
+	Items: []string{
+		"Boss vs Boss",
+		"A Child of the People",
+		"The Derelict Oil Rig",
+		"The Helldrivers",
+		"The Hunger",
+		"The Killing Machine",
+		"The Last Drop",
+		"Lost Treasure",
+		"The Man on the Beach",
+		"Missing Person",
+		"Murder in the Ark",
+		"Mutant Messiah",
+		"The Mysterious Ruin",
+		"A New Kind of Beast",
+		"The Other Person",
+		"Project Sabotaged",
+		"Revolution!",
+		"The Sinkhole",
+		"Starfall",
+		"The Wanderers",
+		"The Zone Cult",
+		"Zone Fever",
+		"Zone-Ghouls Attack",
+		"The Zone Monster",
+		"The Zone Quakes",
+	},
+}
